4d27c347d24d126d47361cb0ef3756a11c1f75b5
wasaw <merenkovalexander@gmail.com>
2024-02-19 11:09:03 +0300

    create clinics page

---
+++ src/pages/Clinics.vue
@@ -0,0 +1,64 @@
+<script>
+import axios from "axios"
+import router from "@/router"
+
+export default {
+    components: {
+        router
+    },
+    data() {
+        return {
+            clinics: []
+        }
+    },
+    mounted() {
+        axios.get('https://dev-app.rnova.org/api/public/getClinics/?api_key=8471e36fd1d7d22996278025475d6593')
+        .then(res => res.data)
+        .then(result => {
+            this.clinics = result.data
+        })
+    },
+}
+</script>
+
+<template>
+    <main>
+        <h2>Шаг 1: Выбор клиники</h2>
+        <div class="buttons" v-for="clinic in clinics" :key="clinic.id">
+            <button>{{ clinic.title }}</button>
+        </div>
+    </main>
+</template>
+
+<style>
+body {
+    background: #232323;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 100%;
+    height: 100vh;
+    overflow: hidden;
+    font-family: 'Helvetica', 'Arial', sans-serif;
+}
+main {
+    max-width: 600px;
+    background: antiquewhite;
+    padding: 50px;
+    border-radius: 15px;
+}
+h2 {
+    text-align: center;
+    margin-bottom: 30px;
+    font-weight: bold;
+    font-size: 22px;
+}
+.buttons {
+    display: flex;
+    flex-direction: column;
+    gap: 15px;
+}
+button {
+    font-size: 18px;
+}
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,21 @@
+import { createWebHistory, createRouter } from "vue-router";
+
+const routes = [
+    {
+        path: '/',
+        name: 'Clinics',
+        component: () => import('../pages/Clinics.vue')
+    }, 
+    // {
+    //     path: '/doctors',
+    //     name: 'doctors',
+    //     component: () => import('../pages/Doctors.vue')
+    // }
+]
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes
+})
+
+export default router
