ae3fa0815179933d4b5db891666a2df5928109d8
wasaw <merenkovalexander@gmail.com>
2024-02-19 12:19:32 +0300

    create doctors page

--- src/pages/Doctors.vue
+++ src/pages/Doctors.vue
@@ -1,15 +1,63 @@
 <template>
-    <h1>Doctors</h1>
+    <main>
+        <h2>Врачи</h2>
+        <div 
+        class="buttons" 
+        v-for="user in users" 
+        :key="user.id">
+            <button>{{ user.name }}</button>
+        </div>
+    </main>
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
+    data() {
+        return {
+            users: []
+        }
+    },
     mounted() {
-        console.log('load')
-        console.log(this.$route.params.id)
+        axios.get('https://dev-app.rnova.org/api/public/getUsers/?api_key=8471e36fd1d7d22996278025475d6593')
+        .then(res => res.data)
+        .then(result => {
+            this.users = result.data.filter(element => element.clinic.includes(this.$route.params.id))
+        })
     }
 }
 </script>
 
 <style>
+body {
+    background: #232323;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 100%;
+    height: 100vh;
+    overflow: hidden;
+    font-family: 'Helvetica', 'Arial', sans-serif;
+}
+main {
+    max-width: 600px;
+    background: antiquewhite;
+    padding: 50px;
+    border-radius: 15px;
+}
+h2 {
+    text-align: center;
+    margin-bottom: 30px;
+    font-weight: bold;
+    font-size: 22px;
+}
+.buttons {
+    display: flex;
+    flex-direction: column;
+    gap: 15px;
+}
+button {
+    font-size: 18px;
+}
 </style>
